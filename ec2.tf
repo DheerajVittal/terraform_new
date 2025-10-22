317447aad3979ee4cae323f89fda8854923e5b4f
DheeraVittal <dheerajbv@gmail.com>
2025-10-22 20:39:52 +0530

    varcreated

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "testing" {
-  ami           = "ami-09c813fb71547fc4f" # Replace with a valid AMI ID for your region
-  instance_type = "t3.micro"
+  ami           = "var.ami_id" # Replace with a valid AMI ID for your region
+  instance_type = "var.instance_type"
   #key_name      = "my-key-pair" # Replace with your SSH key pair name
   #subnet_id     = var.subnet_id # Replace with your Subnet ID or a variable
   vpc_security_group_ids = [aws_security_group.allow_all.id]
