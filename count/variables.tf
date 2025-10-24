84ad39c9896b9938f46f842e72d8c72c06af2836
DheeraVittal <dheerajbv@gmail.com>
2025-10-24 21:08:41 +0530

    count commit

---
+++ count/variables.tf
@@ -0,0 +1,11 @@
+variable "instances" {
+   default = ["mongodb", "redis", "mysql", "rabbitmq"]
+}
+
+variable "zone_id" {
+  default = "Z07037791W1K12HBUGZ67"
+}
+
+variable "domain_name" {
+   default = "nidhirohan.store"
+}
