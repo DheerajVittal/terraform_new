acbdda8b68c522add40db64f91bb82842ef2d9f7
DheeraVittal <dheerajbv@gmail.com>
2025-10-27 21:27:18 +0530

    locals

---
+++ Locals/ec2.tf
@@ -0,0 +1,68 @@
+resource "aws_instance" "terraform" {
+    ami = local.ami_id
+    instance_type = local.instance_type
+    vpc_security_group_ids = [aws_security_group.allow_all.id]
+    tags = local.ec2_tags
+}
+
+resource "aws_security_group" "allow_all" {
+  name   = "${local.common_name}-allow-all"
+
+  # egress {
+  #   from_port        = 0 # from port 0 to to port 0 means all ports
+  #   to_port          = 0 
+  #   protocol         = "-1" # -1 means all protocols
+  #   cidr_blocks      = ["0.0.0.0/0"] # internet
+  # }
+
+  # ingress {
+  #   from_port        = 0 # from port 0 to to port 0 means all ports
+  #   to_port          = 0 
+  #   protocol         = "-1" # -1 means all protocols
+  #   cidr_blocks      = ["0.0.0.0/0"] # internet
+  # }
+
+  # tags = {
+  #   Name = "allow-all"
+  # }
+
+tags = merge(
+    var.common_tags,
+  {
+ Name = "${local.common_name}-allow-all"
+  }
+ )
+}
+
+# resource "aws_instance" "terraform" {
+#     ami = local.ami_id
+#     instance_type = local.instance_type
+#     vpc_security_group_ids = [aws_security_group.allow_all.id]
+#     tags = local.ec2_tags
+# }
+
+# resource "aws_security_group" "allow_all" {
+#   name   = "${local.common_name}-allow-all"
+
+#   egress {
+#     from_port        = var.egress_from_port
+#     to_port          = var.egress_to_port
+#     protocol         = "-1" # -1 means all protocols
+#     cidr_blocks      = var.cidr
+#   }
+
+#   ingress {
+#     from_port        = var.ingress_from_port
+#     to_port          = var.ingress_to_port
+#     protocol         = var.protocol
+#     cidr_blocks      = var.cidr
+#   }
+
+#   tags = merge(
+#       var.common_tags,
+#       {
+#         Name = "${local.common_name}-allow-all"
+#         Purpose = "locals"
+#       }
+#     )
+# }
